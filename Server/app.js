50cefe504cc6b37b0522161080ac46664449765d
Shekher Mashankar <57526384+shekharmashankar@users.noreply.github.com>
2021-12-26 07:56:34 +0530

    Create app.js

---
+++ Server/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const { MONGOURI } = require("./config/keys");
+// const mongodb = require("mongodb");
+
+const PORT = process.env.PORT || 4000
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected',()=>{
+  console.log("Connected to Mongo");
+})
+mongoose.connection.on('error',(err)=>{
+  console.log("Error in connecting ",err);
+})
+
+require('./models/user');
+require('./models/post');
+
+app.use(express.json());
+app.use(require('./routes/login'));
+app.use(require('./routes/post'));
+app.use(require('./routes/user'));
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+app.listen(PORT, function(){
+  console.log("Server is running on Port 4000");
+})
